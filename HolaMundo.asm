14ffc899a248dd54472b2046a5ee821ba32f945e
paem <paulelizaur@hotmail.com>
2025-03-09 16:01:04 -0300

    Update HolaMundo.asm

--- HolaMundo.asm
+++ HolaMundo.asm
@@ -1,22 +1,30 @@
-.model small
-.stack 100h
-.data
-    hola db "Hola Mundo!$"  ; Cadena de texto con terminador '$' requerido por DOS
+.MODEL SMALL ;Define el modelo de memoria pequenio   
 
-.code
-main proc
-    mov ax, @data  ; Cargar el segmento de datos     
-    mov ds, ax     ; Moverlo a DS
+.STACK 100h  ;Reserva espacio para la pila  
 
-    mov ah, 09h    ; Funcion 09h: imprimir cadena en DOS   
-    lea dx, hola   ; Cargar la direccion de la cadena en DX  
+.DATA  ;Seccion de datos donde se definen variables y mensajes 
+ 
+    mensaje DB 'Hola, mundo$' ;Define la cadena de texto a mostrar terminada con '$'  
+      
     
-    int 21h        ; Llamada a interrupcion del DOS
-    mov ah, 4Ch    ; Funcion 4Ch: terminar programa correctamente
-    int 21h
+.CODE  ;Seccion de codigo donde van las instrucciones del programa   
 
-main endp
-end main
+    MOV AX, @DATA  ; Carga la direccion de la seccion de datos en AX  
+    
+    MOV DS, AX ; Mueve la direccion de datos al segmento de datos (DS) 
+
+    MOV DX, OFFSET mensaje ; Carga la direccion del mensaje en DX  
+    
+    MOV AH, 09h  ; Funcion de interrupcion 21h para mostrar texto  
+      
+    INT 21h ; Llama a la interrupcion del sistema para imprimir el mensaje  
+
+    MOV AX, 4C00h ; Codigo para terminar el programa correctamente 
+     
+    INT 21h   ; Llama a la interrupción del sistema para salir 
+    
+END ; Indica el final del programa 
+
 
 
 
